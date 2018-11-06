b04ec77d6502ffca092c66d78f1d1f9b8798aee9
MichaelPerkins2 <michael.perkins.d@gmail.com>
2018-11-06 12:13:05 -0700

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellendus fuga incidunt tempora tenetur quo quod iure ducimus velit at repudiandae, officia autem quam possimus voluptates provident ea. Temporibus, provident.</p>
+        </div>
+    )
+}
+
+export default Rainbow(About);
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Contact = (props) => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellendus fuga incidunt tempora tenetur quo quod iure ducimus velit at repudiandae, officia autem quam possimus voluptates provident ea. Temporibus, provident.</p>
+        </div>
+    )
+}
+
+export default Contact;
